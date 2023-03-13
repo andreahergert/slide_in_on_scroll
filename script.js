8e8fd1f4c8910a7aacb9b256106ce2937a9b293b
Andrea Hergert <ahergert24@hotmail.com>
2023-03-13 14:47:27 -0700

    added functioniality and screenshot

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
@@ -13,3 +13,20 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if(isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active');
+      } else {
+        sliderImage.classList.remove('active');
+      }
+    })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
